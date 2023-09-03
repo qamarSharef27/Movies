d5a94e535d03cfc0119c6b63970897e6b2507a60
qamarSharef27 <qamarqoqo0@gmail.com>
2023-09-03 17:29:33 +0300

    Implements the Search Movies app functionality that ensures a comprehensive and user-friendly experience such as: >> The user can see the movies without searching, search for movies by title, a responsive design that looks good on desktop and mobile devices.

--- main.js
+++ main.js
@@ -4,10 +4,11 @@ const url = 'https://imdb188.p.rapidapi.com/api/v1/searchIMDB?query=*';
 const options = {
 	method: 'GET',
 	headers: {
-		'X-RapidAPI-Key': 'b0770d5857mshc600ad7208fbf5cp1c3b22jsnb46c8b13d3e0',
+		'X-RapidAPI-Key': '65fee9fa11msh3dbb751cc4b151fp1b0d12jsn33a61633b290',
 		'X-RapidAPI-Host': 'imdb188.p.rapidapi.com'
 	}
 };
+
 
 const ResultDiv = document.querySelector(".searchResults");
 const searchHistory = document.getElementById('searchHistory');
@@ -15,36 +16,21 @@ const searchHistory = document.getElementById('searchHistory');
 let movies = [];
 
 function MovieDetails(movieName) {
-    ResultDiv.innerHTML = ""; 
+  ResultDiv.innerHTML = "";
 
   fetch(`https://imdb188.p.rapidapi.com/api/v1/searchIMDB?query=${movieName}`, options)
     .then(response => response.json())
     .then(({ data: list }) => {
-
       list.forEach(({ title: movieTitle, image: poster, stars }) => {
         const picture = `<img src="${poster}" alt="movie" class="picture">`;
         const description = `<div class="description"><p><b> Movie Name: </b> ${movieTitle } <br></br> <b> Movie Stars: </b> ${stars} <br></p></div>`;
         const newDiv = `<div class="new">${picture + description}</div>`;
         ResultDiv.innerHTML += newDiv;
       });
+
+      localStorage.setItem("movies", JSON.stringify([movieName]));
     })
     .catch(err => console.log(err));
-}
-
-const storedMovies = JSON.parse(localStorage.getItem("movies")) || [];
-
-function displayStoredMovies() {
-  ResultDiv.innerHTML = ""; 
-
-  storedMovies.forEach(movieTitle => {
-    MovieDetails(movieTitle);
-  });
-}
-
-if (storedMovies.length === 0) {
-  MovieDetails("*");
-} else {
-  displayStoredMovies();
 }
 
 const searchInput = document.getElementById("searchValue");
@@ -54,14 +40,24 @@ searchInput.addEventListener("keyup", (event) => {
     event.preventDefault();
 
     const movieTitle = searchInput.value.trim();
-    storedMovies.unshift(movieTitle);
 
-    localStorage.setItem("movies", JSON.stringify(storedMovies));
+    localStorage.clear();
+    MovieDetails(movieTitle);
 
-    displayStoredMovies();
+    searchInput.placeholder = movieTitle;
+
   }
 });
 
 searchInput.addEventListener("click", (event) => {
   searchInput.value = "";
 });
+
+const storedMovies = JSON.parse(localStorage.getItem("movies")) || [];
+
+if (storedMovies.length === 0) {
+  MovieDetails("*");
+} else {
+  MovieDetails(storedMovies[0]);
+  searchInput.placeholder = storedMovies[0];
+}
